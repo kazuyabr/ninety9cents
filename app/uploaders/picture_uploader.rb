00750c2d236b85182818e8b65c70276cab723b27
Michael Kempster <michaelkempster@bigpond.com>
2014-12-18 14:13:36 +1100

    Cloudinary setup

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,12 +2,14 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
 
+  include Cloudinary::CarrierWave if Rails.env.production?
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :file if Rails.env.development?
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
